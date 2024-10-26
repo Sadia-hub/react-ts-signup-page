9f6d40fd5cc0915599f69e8dd724795f23eab71b
Sadia-hub <57145516+Sadia-hub@users.noreply.github.com>
2024-10-26 18:55:43 +0400

    signin page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 
 import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      {/* <SignUp /> */}
+      <SignIn />
     </div>
   );
 }
